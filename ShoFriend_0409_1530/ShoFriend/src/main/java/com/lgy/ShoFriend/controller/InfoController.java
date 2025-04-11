23e86dc8f6940702d5954545b8491bbbea2da3ca
Kwon <kwonjw5191@gmail.com>
2025-04-12 01:04:21 +0900

    feat: 로그 추가.

--- ShoFriend_0409_1530/ShoFriend/src/main/java/com/lgy/ShoFriend/controller/InfoController.java
+++ ShoFriend_0409_1530/ShoFriend/src/main/java/com/lgy/ShoFriend/controller/InfoController.java
@@ -43,6 +43,7 @@ public class InfoController {
 	public String updatePwdOk(@RequestParam("new_pwd") String newPwd, HttpSession session) {
 	    CustomerDTO loginCustomer = (CustomerDTO) session.getAttribute("loginCustomer");
 
+		log.info("안녕");
 	    if (loginCustomer != null) {
 	        HashMap<String, String> param = new HashMap<String, String>();
 	        param.put("id", Integer.toString(loginCustomer.getId()));
@@ -54,6 +55,7 @@ public class InfoController {
 	        
 	        return "redirect:customer_info";
 	    } else {
+			log.info("잘가");
 	        return "redirect:login";
 	    }
 	}
